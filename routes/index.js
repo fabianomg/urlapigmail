fe600f1043c177808804155844ed4c908adc104d
fabianomg <fabianodasilva496@gmail.com>
2020-08-16 11:43:35 -0300

    add config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
+const querystring = require('querystring')
 
+function fullUrl(req) {
+  return url.format({
+    protocol: req.protocol,
+    host: req.get('host'),
+    pathname: req.originalUrl
+  });
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'teste' });
+router.get('/', function (req, res, next) {
+  let url = fullUrl(req)
+  let c = querystring.decode(url)
+  let code = Object.keys(c)
+  res.render('index', { title: 'Api Gmail', code: code });
 });
 
 module.exports = router;
